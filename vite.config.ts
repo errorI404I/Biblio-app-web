44db9993c3941987e11b898edc90941a67e1691d
error|404| <ignacio.dellac@gmail.com>
2026-08-04 02:18:16 -0300

    version prueba 0.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tanstackStart(), react()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-});
+  plugins: [
+    tsconfigPaths(),
+    TanStackRouterVite(),
+    react(),
+    tailwindcss(),
+  ],
+})
